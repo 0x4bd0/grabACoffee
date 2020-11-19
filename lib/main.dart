8bbf91572632c76b236975d7ceea8904450cc121
0x4bd0 <abdoumjr@gmail.com>
2020-11-19 11:18:36 +0100

    setupd firebase android

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_app/screens/wrapper.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      
+      home : Wrapper()
     )
   );
 }
---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase_app/screens/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    
+    return Home();
+
+  }
+}

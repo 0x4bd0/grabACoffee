46991818ea326b9618715d06a576262643545793
0x4bd0 <abdoumjr@gmail.com>
2020-11-21 16:39:35 +0100

    fix config issues

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_firebase_app/screens/home/home.dart';
 import 'package:flutter_firebase_app/screens/auth/auth.dart';
 
 class Wrapper extends StatelessWidget {
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter_firebase_app/services/firebase_auth.dart';
 import 'package:flutter_firebase_app/models/user.dart';
 
 
@@ -7,14 +6,23 @@ class AuthService {
 
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  UserModel _user(UserCredential user){
-  return user != null ? UserModel(uid:user.user.uid) : null;
+  // create user model
+  UserModel _user(FirebaseUser user){
+  return user != null ? UserModel(uid:user.uid) : null;
   }
  
+
+
+  //auth change stream
+  Stream<UserModel> get user {
+    return _auth.onAuthStateChanged.map(_user);
+  }
+  
   // sign in 
   Future singInAnony() async {
     try {
-    UserCredential user = await _auth.signInAnonymously();
+    AuthResult res = await _auth.signInAnonymously();
+    FirebaseUser user = res.user;
     return _user(user);
     } 
     catch(err) {
